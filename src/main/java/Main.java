0acbadae6714ccbc226792f79455fd7dd9f8091b
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-26 18:25:36 -0400

    Implementing deposits and withdrawals with threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Savings Account Threading Example");
+
+        SavingsAccount oscarSavingsAcc = new SavingsAccount();
+
+        // Initial amount
+        oscarSavingsAcc.deposit(1000000);
+
+        // Create Threads
+        List<Thread> transactionArray = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            if (i % 2 == 0) {
+                transactionArray.add(new Thread(() -> loopDeposits(oscarSavingsAcc)));
+            } else {
+                transactionArray.add(new Thread(() -> loopWithdrawals(oscarSavingsAcc)));
+            }
+        }
+
+        // Run all threads
+        for (Thread t : transactionArray){
+            t.start();
+        }
+
+        // Run join() on the threads in order for the main program to
+        // complete and print final total in correct sequence
+        for (Thread t : transactionArray){
+            t.join();
+        }
+
+        // At current setup, should end up being the same as initial amount
+        System.out.println("FINAL TOTAL => " + oscarSavingsAcc.getTotal());
+    }
+
+    public static void loopWithdrawals(SavingsAccount acc) {
+        System.out.println("Withdrawals thread running...");
+        int transactions = 10000, amount = 100, sleep = 100;
+
+        for (int j = 0; j < transactions; j++) {
+            acc.withdraw(amount);
+            // accountSleep(sleep);
+        }
+    }
+
+    public static void loopDeposits(SavingsAccount acc) {
+        System.out.println("Deposits thread running...");
+        int transactions = 10000, amount = 100, sleep = 90;
+
+        for (int i = 0; i < transactions; i++) {
+            acc.deposit(amount);
+            // accountSleep(sleep);
+        }
+    }
+
+    static void accountSleep(int sleep) {
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,16 +1,27 @@
 public class SavingsAccount {
 
-  private long total = 0;
+    private long total = 0;
 
-  public boolean withdraw(long amount) {
-      
-  }
+    public synchronized boolean withdraw(long amount) {
+        // why return boolean? how can I use this on main?
+        // Should I let my total be negative?
+        if (amount > total) {
+            // If this case hits, final amount won't be the initial amount
+            System.out.println("not enough balance... transaction canceled");
+            return false;
+        } else {
+            total -= amount;
+            System.out.println("withdrew => " + amount + " || Total: " + getTotal());
+            return true;
+        }
+    }
 
-  public void deposit(long amount) {
+    public synchronized void deposit(long amount) {
+        total += amount;
+        System.out.println("deposited => " + amount + " || Total: " + getTotal());
+    }
 
-  }
-
-  public long getTotal() {
-      return total;
-  }
+    public long getTotal() {
+        return total;
+    }
 }
